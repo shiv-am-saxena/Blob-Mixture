fcaabaf59b8212a1ca647a855d0afd5152b41fc7
Shivam Saxena <shivam2003official@gmail.com>
2025-09-19 18:39:33 +0530

    feat: add Firefly blob configuration with updated properties

--- public/configVals.js
+++ public/configVals.js
@@ -17,6 +17,11 @@ const blobs = [
         config: { "uPositionFrequency": 0.584, "uPositionStrength": 0.276, "uSmallWavePositionFrequency": 0.899, "uSmallWavePositionStrength": 1.266, "roughness": 0, "metalness": 1, "envMapIntensity": 2, "clearcoat": 0, "clearcoatRoughness": 0, "transmission": 0, "flatShading": false, "wireframe": false, "map": "purple-rain" },
     },
     {
+        name: 'Firefly',
+        background: '#380b16',
+        config: { "uPositionFrequency": 1, "uPositionStrength": 1, "uSmallWavePositionFrequency": 2.3, "uSmallWavePositionStrength": 0.1, "roughness": 1, "metalness": 0, "envMapIntensity": 0.5, "clearcoat": 0, "clearcoatRoughness": 0, "transmission": 0, "flatShading": false, "wireframe": false, "map": "passion" },
+    },
+    {
         name: 'Alien Goo',
         background: '#45ACD8',
         config: { "uPositionFrequency": 1.022, "uPositionStrength": 0.99, "uSmallWavePositionFrequency": 0.378, "uSmallWavePositionStrength": 0.341, "roughness": 0.292, "metalness": 0.73, "envMapIntensity": 0.86, "clearcoat": 1, "clearcoatRoughness": 0, "transmission": 0, "flatShading": false, "wireframe": false, "map": "lucky-day" },
